f23131c89cc2351aff81644294bfee3ea25e0aab
Itamar Ratson <itamar.ratson@gmail.com>
2026-02-24 14:41:18 +0200

    revert(vault): remove non-existent -dev-storage flag from transit vault
    The -dev-storage=file flag (added in 0d04163) does not exist in any
    Vault version. Revert to plain -dev mode; transit engine is recreated
    by terragrunt apply after container restarts.

--- terraform/modules/vault/transit/main.tf
+++ terraform/modules/vault/transit/main.tf
@@ -31,7 +31,7 @@ resource "docker_container" "vault" {
     add = ["CAP_IPC_LOCK"]
   }
 
-  command = ["server", "-dev", "-dev-storage=file"]
+  command = ["server", "-dev"]
   restart = "unless-stopped"
 
   # Resource limits - actual usage ~73 MiB / <1% CPU
